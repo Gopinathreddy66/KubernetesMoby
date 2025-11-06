50a5dfb4836d328d7379882daa6a0c35524c9398
Rafael Camelo <rafaelcamelo31@gmail.com>
2025-11-05 20:28:27 -0400

    refactor validations and swagger generation flow
    Signed-off-by: Rafael Camelo <rafaelcamelo31@gmail.com>

--- api/scripts/validate-swagger.sh
+++ api/scripts/validate-swagger.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 set -e
-SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${SCRIPTDIR}/.validate"
 
-IFS=$'\n'
-files=($(validate_diff --diff-filter=ACMR --name-only -- 'api/swagger.yaml' || true))
-unset IFS
+# Expected to be in api directory
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-if [ -n "${TEST_FORCE_VALIDATE:-}" ] || [ ${#files[@]} -gt 0 ]; then
-	yamllint -f parsable -c "${SCRIPTDIR}"/yamllint.yaml api/swagger.yaml
-	if out=$(swagger validate api/swagger.yaml); then
-		echo "Congratulations!  ${out}"
-	else
-		echo "${out}" >&2
-		false
-	fi
+echo "Validating swagger.yaml..."
+
+yamllint -f parsable -c validate/yamllint.yaml swagger.yaml
+
+if out=$(swagger validate swagger.yaml); then
+	echo "Validation done! ${out}"
+else
+	echo "${out}" >&2
+	false
 fi
